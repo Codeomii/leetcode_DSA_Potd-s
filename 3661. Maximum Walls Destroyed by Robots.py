b8953cd366d32f6f73900a98f5ffb58c73d9bf9a
Codeomii <omp83075@gmail.com>
2026-04-04 21:33:14 +0530

    03-04-2026

--- 3661. Maximum Walls Destroyed by Robots.py
+++ 3661. Maximum Walls Destroyed by Robots.py
@@ -58,63 +58,33 @@ All values in walls are unique'''
 
 # Solution :--
 
+import bisect
+from typing import List
+
 class Solution:
-    def maxWalls(
-        self, robots: List[int], distance: List[int], walls: List[int]
-    ) -> int:
-        n = len(robots)
-        left = [0] * n
-        right = [0] * n
-        num = [0] * n
-        robots_to_distance = {}
-
-        for i in range(n):
-            robots_to_distance[robots[i]] = distance[i]
-
+    def maxWalls(self, robots: List[int], distance: List[int], walls: List[int]) -> int:
+        dist_map = {r: d for r, d in zip(robots, distance)}
         robots.sort()
         walls.sort()
-
+        n = len(robots)
+        
+        L, R, B = [0] * n, [0] * n, [0] * n
         for i in range(n):
-            pos1 = bisect.bisect_right(walls, robots[i])
+            r, d = robots[i], dist_map[robots[i]]
+            
+            l_bound = max(r - d, robots[i-1] + 1) if i > 0 else r - d
+            L[i] = bisect.bisect_right(walls, r) - bisect.bisect_left(walls, l_bound)
+            
+            r_bound = min(r + d, robots[i+1] - 1) if i < n - 1 else r + d
+            R[i] = bisect.bisect_right(walls, r_bound) - bisect.bisect_left(walls, r)
+            
+            if i > 0:
+                B[i] = bisect.bisect_right(walls, robots[i]) - bisect.bisect_left(walls, robots[i-1])
 
-            if i >= 1:
-                left_bound = max(
-                    robots[i] - robots_to_distance[robots[i]], robots[i - 1] + 1
-                )
-                left_pos = bisect.bisect_left(walls, left_bound)
-            else:
-                left_pos = bisect.bisect_left(
-                    walls, robots[i] - robots_to_distance[robots[i]]
-                )
-
-            left[i] = pos1 - left_pos
-
-            if i < n - 1:
-                right_bound = min(
-                    robots[i] + robots_to_distance[robots[i]], robots[i + 1] - 1
-                )
-                right_pos = bisect.bisect_right(walls, right_bound)
-            else:
-                right_pos = bisect.bisect_right(
-                    walls, robots[i] + robots_to_distance[robots[i]]
-                )
-
-            pos2 = bisect.bisect_left(walls, robots[i])
-            right[i] = right_pos - pos2
-
-            if i == 0:
-                continue
-
-            pos3 = bisect.bisect_left(walls, robots[i - 1])
-            num[i] = pos1 - pos3
-
-        sub_left, sub_right = left[0], right[0]
+        dp_l, dp_r = L[0], R[0]
         for i in range(1, n):
-            current_left = max(
-                sub_left + left[i],
-                sub_right - right[i - 1] + min(left[i] + right[i - 1], num[i]),
-            )
-            current_right = max(sub_left + right[i], sub_right + right[i])
-            sub_left, sub_right = current_left, current_right
-
-        return max(sub_left, sub_right)
+            new_l = max(dp_l + L[i], (dp_r - R[i-1]) + min(L[i] + R[i-1], B[i]))
+            new_r = max(dp_l, dp_r) + R[i]
+            dp_l, dp_r = new_l, new_r
+            
+        return max(dp_l, dp_r)
