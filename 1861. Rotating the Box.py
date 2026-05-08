8123b34c0ddf65d9c68dfc30968065f88117bb7f
Codeomii <omp83075@gmail.com>
2026-05-08 13:50:52 +0530

    06-05-2026

---
+++ 1861. Rotating the Box.py
@@ -0,0 +1,72 @@
+'''1861. Rotating the Box:--
+
+You are given an m x n matrix of characters boxGrid representing a side-view of a box. Each cell of the box is one of the following:
+
+A stone '#'
+A stationary obstacle '*'
+Empty '.'
+The box is rotated 90 degrees clockwise, causing some of the stones to fall due to gravity. Each stone falls down until it lands on an obstacle, another stone, or the bottom of the box. Gravity does not affect the obstacles' positions, and the inertia from the box's rotation does not affect the stones' horizontal positions.
+
+It is guaranteed that each stone in boxGrid rests on an obstacle, another stone, or the bottom of the box.
+
+Return an n x m matrix representing the box after the rotation described above.
+
+ 
+
+Example 1:
+
+
+
+Input: boxGrid = [["#",".","#"]]
+Output: [["."],
+         ["#"],
+         ["#"]]
+Example 2:
+
+
+
+Input: boxGrid = [["#",".","*","."],
+              ["#","#","*","."]]
+Output: [["#","."],
+         ["#","#"],
+         ["*","*"],
+         [".","."]]
+Example 3:
+
+
+
+Input: boxGrid = [["#","#","*",".","*","."],
+              ["#","#","#","*",".","."],
+              ["#","#","#",".","#","."]]
+Output: [[".","#","#"],
+         [".","#","#"],
+         ["#","#","*"],
+         ["#","*","."],
+         ["#",".","*"],
+         ["#",".","."]]
+ 
+
+Constraints:
+
+m == boxGrid.length
+n == boxGrid[i].length
+1 <= m, n <= 500
+boxGrid[i][j] is either '#', '*', or '.'.'''
+
+# Solution:--
+
+class Solution:
+    def rotateTheBox(self, box):
+        m, n = len(box), len(box[0])
+        
+        for row in box:
+            empty = n - 1
+            for j in range(n - 1, -1, -1):
+                if row[j] == '*':
+                    empty = j - 1
+                elif row[j] == '#':
+                    row[j], row[empty] = '.', '#'
+                    empty -= 1
+        
+
+        return list(zip(*box[::-1]))
